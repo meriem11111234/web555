fd620003ed681fec396e983f6350877e341a9780
DjabelaMailys <djabelamailys01@gmail.com>
2025-04-01 22:54:48 +0200

    Ajouter CSS et Bootstrap

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
 
 // Configuration d'EJS
 app.set("view engine", "ejs");
